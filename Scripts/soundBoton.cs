116783333f0ea4a1443e8120d85da1e286b18788
Jorge Ernesto Nicho Galagarza <40152300+THEFLILUX@users.noreply.github.com>
2023-09-30 13:54:29 -0500

    Sound

---
+++ Scripts/soundBoton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu: MonoBehaviour
+{
+
+    public AudioSource audio;
+
+    public void playButton()
+    {
+        audio.Play();
+    }
+}
